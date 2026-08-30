73e7a69a940848f0fb159de69adafc2ede6767d2
Beni <bnayahu@arena-digital.com>
2026-08-30 11:24:08 +0300

    drills package internal

---
+++ Assets/Scripts/Drills-Package/CharComponent.cs
@@ -0,0 +1,166 @@
+using Sirenix.OdinInspector; 
+using UnityEngine;
+using UnityEngine.Animations;  
+using UnityEngine.Playables;
+#if DRILL_EXPORT_EDITOR
+[ExecuteInEditMode]
+#endif
+[SelectionBase]
+public class CharComponent : MonoBehaviour {
+    [System.Serializable]
+    public class CharSkin {
+        public Animator Animator;
+        public SkinnedMeshRenderer ShirtRenderer;
+        public Transform HeadTf;
+        [Range(0f, 100f)] public float Weight = 100f;
+    } 
+
+    static readonly int AV_MirrorHash = Animator.StringToHash("is_mirror");
+    static readonly int AV_CycleOffset = Animator.StringToHash("cycle_offset");
+    static readonly Color _EnemyColor = Color.softRed;
+    static readonly Color _AllyColor = Color.white;
+    static bool IsExportEditor =>
+#if DRILL_EXPORT_EDITOR
+        !Application.isPlaying;
+#else
+        false;
+#endif
+    [ShowInInspector, SerializeField] CharData _data;
+    [SerializeField] CharSkin[] _skins;
+
+    Animator _anim => _skins[_usedAnimatorSkinIdx].Animator;
+    public CharSkin ActiveSkin => _skins[_usedAnimatorSkinIdx];
+
+    int _usedAnimatorSkinIdx = 0; 
+    public CharData Data => _data; 
+    [ShowInInspector]
+    public AnimationClip Clip => _overrideAnimation ? _overrideAnimation.animationClips[0] : null;  
+    public void SetData(CharData data, bool mirror) {
+        _data = data; 
+        if (IsExportEditor && !_graph.IsValid())
+            RecreateGraph();
+
+        if(data != null) { 
+            var pos = new Vector3(data.FieldStandardPosition.y,0,data.FieldStandardPosition.x);
+            var rot = Quaternion.Euler(0, data.yRotation, 0);
+            transform.SetLocalPositionAndRotation(pos, rot);
+            SetAnim(data.Animation);
+            if(_controllerPlayable.IsValid())
+                _controllerPlayable.SetBool(AV_MirrorHash, mirror);
+                
+            foreach(var skin in _skins) {
+                var renderer = skin.ShirtRenderer;
+                var material = IsExportEditor && ! Application.isPlaying ? renderer.sharedMaterial : renderer.material;
+                material.color = data.IsFriendly ? _AllyColor : _EnemyColor;
+            }
+        }
+    } 
+
+    public void RandomizeSkin() {
+        if (_skins.Length > 0) {
+            float totalWeight = 0f;
+            foreach (var skin in _skins)
+                totalWeight += skin.Weight;
+            
+            float roll = Random.Range(0f, totalWeight);
+            float accumulated = 0f;
+            _usedAnimatorSkinIdx = _skins.Length - 1;
+            for (int i = 0; i < _skins.Length; i++) {
+                accumulated += _skins[i].Weight;
+                if (roll < accumulated) {
+                    _usedAnimatorSkinIdx = i;
+                    break;
+                }
+            }
+
+            for (int i = 0; i < _skins.Length; i++) {
+                _skins[i].Animator.gameObject.SetActive(i == _usedAnimatorSkinIdx);
+            }
+            RecreateGraph(); 
+        } else {
+            Debug.LogError("No animator skins assigned to CharComponent.");
+        }
+    }
+
+    PlayableGraph _graph;
+    AnimationPlayableOutput _output; 
+    private void Start() {
+        foreach (var skin in _skins) {
+            skin.ShirtRenderer.sharedMaterial = new Material(skin.ShirtRenderer.sharedMaterial);
+        }
+        RandomizeSkin();
+         
+        foreach (var s in _skins) {
+            s.Animator.keepAnimatorStateOnDisable = true;
+            s.Animator.fireEvents = false;
+            s.Animator.enabled = true;
+            s.Animator.speed = 1f;
+        }
+        RecreateGraph();
+    } 
+    AnimatorControllerPlayable _controllerPlayable;
+    AnimatorOverrideController _overrideAnimation; 
+    void RecreateGraph() {
+        if (!_anim.runtimeAnimatorController) {
+            Debug.LogError("Animator does not have a runtime animator controller assigned. Please assign one in the inspector.");
+            return;
+        }
+
+        if(!_overrideAnimation)
+            _overrideAnimation = new AnimatorOverrideController(_anim.runtimeAnimatorController);
+        if(_anim.runtimeAnimatorController != _overrideAnimation)
+            _anim.runtimeAnimatorController = _overrideAnimation;
+
+        SetAnim(Data?.Animation);
+
+        if (IsExportEditor) {
+            if (_graph.IsValid())
+                _graph.Destroy();
+
+            _graph = PlayableGraph.Create("SingleAnimationGraph");
+            _graph.SetTimeUpdateMode(DirectorUpdateMode.Manual);
+            _controllerPlayable = AnimatorControllerPlayable.Create(_graph, _overrideAnimation);
+            _output = AnimationPlayableOutput.Create(_graph, "AnimationOutput", _anim);
+            _output.SetSourcePlayable(_controllerPlayable);
+            _graph.Play();
+        }
+    }
+
+    private void OnDestroy() {
+        if (IsExportEditor && _graph.IsValid())
+            _graph.Destroy();
+    } 
+
+    public void SetAnimationTime(float time) {
+        if (IsExportEditor) {
+            if (!_graph.IsValid() || !_controllerPlayable.IsValid())
+                return;
+        }
+
+        if (!_overrideAnimation)
+            RecreateGraph();
+
+        var actualTime = time + (_data?.AnimationTimeOffset ?? 0f);
+        var clip = _overrideAnimation.animationClips[0];
+        var duration = clip ? clip.length : 0f;
+        var normalized = (duration == 0f || actualTime < 0f) ? 0f : actualTime / duration;
+
+        if (IsExportEditor) {
+            _controllerPlayable.Play("Clip", 0, normalized);
+            _controllerPlayable.SetFloat(AV_CycleOffset, normalized);
+            _graph.Evaluate();
+        } else {
+            _anim.SetFloat(AV_CycleOffset, normalized);
+            _anim.Update(0f);
+        }
+    } 
+    void SetAnim(AnimationClip clip) {
+        if (clip == Clip)
+            return;
+		
+		if(!_overrideAnimation)
+			RecreateGraph();
+		
+        _overrideAnimation[_overrideAnimation.animationClips[0]] = clip;
+    } 
+}
---
+++ Assets/Scripts/Drills-Package/DrillData.cs
@@ -0,0 +1,66 @@
+using Sirenix.OdinInspector; 
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using System.IO;
+#if UNITY_EDITOR
+using Sirenix.Utilities.Editor;
+#endif
+
+[System.Serializable]
+public class DrillData : ScriptableObject {
+    public enum Category { 
+        PickAndRoll,
+        Shooting,
+        PostPlays
+    }
+    [BoxGroup("Origin")]
+    public Category DrillCategory;
+    [BoxGroup("Origin")]
+    public Vector3 OriginPoint;
+    [BoxGroup("Origin"), PropertyRange(0,360)]
+    public float OriginYRotation;
+    [BoxGroup("Origin")]
+    public bool MirrorLeftRight;
+
+    [BoxGroup("Player Position")]
+    public Vector3 LocalPlayerStartPosition;
+    public Vector3 PlayerStartPosition {
+        get {
+            return 
+                Matrix4x4.TRS(OriginPoint, Quaternion.Euler(0, OriginYRotation, 0), Vector3.one)
+                .MultiplyPoint(LocalPlayerStartPosition);
+        }
+    }
+
+
+    [PropertyOrder(100), ListDrawerSettings(OnBeginListElementGUI = nameof(OnBeginItemGUI), ShowFoldout = false)] 
+    public List<CharData> CharsData = new List<CharData>();
+
+#if UNITY_EDITOR
+    [Button(icon: SdfIconType.HexagonHalf), PropertyOrder( -101)] void DuplicateMirror() {
+        var mirror = Instantiate(this);
+        mirror.name += "_mirror";
+        mirror.OriginPoint.x = -mirror.OriginPoint.x;
+        mirror.OriginYRotation = -mirror.OriginYRotation;
+        foreach(var c in mirror.CharsData) {
+            c.yRotation = -c.yRotation;
+            c.FieldStandardPosition.x = -c.FieldStandardPosition.x;
+        }
+
+        var path = AssetDatabase.GetAssetPath(this);
+        var dir = Path.GetDirectoryName(path);
+        var extention = Path.GetExtension(path);  
+        var newPath = Path.Combine(dir, mirror.name + extention);
+        AssetDatabase.CreateAsset(mirror, newPath); 
+    }
+#endif
+
+    void OnBeginItemGUI(int idx){
+#if UNITY_EDITOR
+        SirenixEditorGUI.Title("Player: " + idx.ToString(), "",TextAlignment.Left, true, true);
+        EditorGUILayout.Space();
+#endif
+    }
+
+}
