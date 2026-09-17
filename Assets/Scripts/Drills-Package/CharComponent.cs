f7d2b227dc0fd65831a44e47e024d110f3435c46
Beni <bnayahu@arena-digital.com>
2026-09-17 20:15:34 +0300

    add hologram

--- Assets/Scripts/Drills-Package/CharComponent.cs
+++ Assets/Scripts/Drills-Package/CharComponent.cs
@@ -1,4 +1,5 @@
-using Sirenix.OdinInspector; 
+using Sirenix.OdinInspector;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Animations;  
 using UnityEngine.Playables;
@@ -27,7 +28,8 @@ public class CharComponent : MonoBehaviour {
 #endif
     [ShowInInspector, SerializeField] CharData _data;
     [SerializeField] CharSkin[] _skins;
-
+    [SerializeField] Material _hologramMat;
+    Dictionary<SkinnedMeshRenderer, Material> _originalMaterials = new();
     Animator _anim => _skins[_usedAnimatorSkinIdx].Animator;
     public CharSkin ActiveSkin => _skins[_usedAnimatorSkinIdx];
 
@@ -87,6 +89,22 @@ public class CharComponent : MonoBehaviour {
             RecreateGraph(); 
         } else {
             Debug.LogError("No animator skins assigned to CharComponent.");
+        }
+    }
+
+    public void SetHologram(bool isHologram) {
+        foreach (var skin in _skins) {
+            var renderer = skin.ShirtRenderer;
+            if (isHologram) {
+                if (!_originalMaterials.ContainsKey(renderer))
+                    _originalMaterials[renderer] = renderer.sharedMaterial;
+                renderer.sharedMaterial = _hologramMat;
+            } else {
+                if (_originalMaterials.TryGetValue(renderer, out var originalMat)) {
+                    renderer.sharedMaterial = originalMat;
+                    _originalMaterials.Remove(renderer);
+                }
+            }
         }
     }
 
